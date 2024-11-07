4712d5e91ded0bfd2b474837c406fb25c4482624
TamamTamar <tamar.tamam770@gmail.com>
2024-11-07 13:44:58 +0200

    c

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { FC } from 'react';
 import { Carousel } from 'flowbite-react';
 import './Carousel.scss';
 
-const CarouselComponent: React.FC = () => {
-    const [isMobile, setIsMobile] = useState<boolean>(false);
-
-    // אפקט לבדיקת גודל המסך
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);  // אם המסך קטן מ-768px, נחשב כמובייל
-        };
-
-        // בדיקה בעת טעינת הרכיב
-        handleResize();
-        // מאזין לשינוי בגודל המסך
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize); // הסרה של המאזין בעת פירוק הרכיב
-        };
-    }, []);
-
+const CarouselComponent: FC = () => {
     return (
         <div className="custom-carousel">
-                <Carousel pauseOnHover>
-                    <img
-                        src="/img/Brown & White Fashion Photo Collage New Collection Facebook Cover (2400 x 1000 px).png"
-                        alt="Elegant Dresses Cover 1"
-                    />
-                    <img
-                        src="/img/banner-2.png"
-                        alt="Second Image for Desktop"
-                    />
-                </Carousel>
-            
+            <Carousel pauseOnHover>
+                <img
+                    src="/img/Brown & White Fashion Photo Collage New Collection Facebook Cover (2400 x 1000 px).png"
+                    alt="Elegant Dresses Cover 1"
+                    className="carousel-image desktop-image"
+                />
+                <img
+                    src="/img/banner-2.png"
+                    alt="Second Image for Desktop"
+                    className="carousel-image desktop-image"
+                />
+            </Carousel>
         </div>
     );
 };
