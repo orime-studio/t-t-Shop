ce2b61696ba43056b69ab9ad1b9eb50dee33c960
TamamTamar <tamar.tamam770@gmail.com>
2024-11-20 12:30:23 +0200

    header

--- src/layout/BeitChabadLayout.tsx
+++ src/layout/BeitChabadLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import ChabadHeader from "../routes/BeitHabad/ChabadHeader";
 
 const BeitChabadLayout = () => {
     return (
         <div className="flex flex-col min-h-screen">
+        <ChabadHeader/>
             <main className="flex-1">
                 <Outlet />
             </main>
---
+++ src/routes/BeitHabad/ChabadHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChabadHeader = () => {
+  return (
+    <header className="bg-orange-100 text-blue-950 dark:bg-gray-700 p-5 dark:text-white text-5xl font-extralight h-33 text-center">
+         
+        </header>
+  )
+}
+
+export default ChabadHeader
