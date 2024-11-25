8baf9cb623e674ade0cb93faeebdf2fad25e308d
sofiyaOsadchi <sofiyahason@gmail.com>
2024-11-25 12:57:42 +0200

    car

---
+++ src/routes/BeitHabad/CarouselChabad.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { Carousel } from 'flowbite-react';
+import './Carousel.scss';
+
+const CarouselChabd: FC = () => {
+    return (
+        <div className="custom-carousel">
+            <Carousel pauseOnHover>
+                <img
+                    src="/img/PikiWiki_Israel_32536_Old_Jaffa.JPG"
+                    alt="Elegant Dresses Cover 1"
+                    className="carousel-image desktop-image"
+                />
+                <img
+                    src="/img/PikiWiki_Israel_34118_Old_Jaffa.JPG"
+                    alt="Second Image for Desktop"
+                    className="carousel-image desktop-image"
+                />
+            </Carousel>
+        </div>
+    );
+};
+
+export default CarouselChabd;
--- src/routes/BeitHabad/HomePage.tsx
+++ src/routes/BeitHabad/HomePage.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import CandleLightingTimes from './CandleLightingTimes'
 import LatestParasha from './LatestParasha'
 import LatestVideo from './LatestVideo'
+import CarouselChabd from './CarouselChabad'
 
 const HomePage = () => {
   return (
     <div>
+      <CarouselChabd />
       <CandleLightingTimes />
       <LatestParasha />
       <LatestVideo />
