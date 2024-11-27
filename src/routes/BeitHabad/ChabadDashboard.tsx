1db82db5ff8dddb445cfce0f28c68c5b264176e0
TamamTamar <tamar.tamam770@gmail.com>
2024-11-27 12:22:16 +0200

    b

--- src/routes/BeitHabad/ChabadDashboard.tsx
+++ src/routes/BeitHabad/ChabadDashboard.tsx
@@ -1,11 +1,10 @@
 import { Tabs } from "flowbite-react";
 import { FiBox } from "react-icons/fi";
-import './ChabadDashboard.scss';
 import AdminParashot from "./AdminParashot";
 
 const ChabadDashboard = () => {
     return (
-        <Tabs dir="rtl" aria-label="Tabs with icons" className="tabs rtl-tabs">
+        <Tabs aria-label="Tabs with icons" className="tabs">
             <Tabs.Item active title="נתוני פרשות" icon={FiBox}>
                 <AdminParashot />
             </Tabs.Item>
