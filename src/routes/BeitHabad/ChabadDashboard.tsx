4c0dff62e9688d7571728395a510e9e41158f1a7
TamamTamar <tamar.tamam770@gmail.com>
2024-11-27 12:17:18 +0200

    b

--- src/routes/BeitHabad/ChabadDashboard.tsx
+++ src/routes/BeitHabad/ChabadDashboard.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "flowbite-react";
 import { FiBox } from "react-icons/fi";
+import './ChabadDashboard.scss';
 import AdminParashot from "./AdminParashot";
 
 const ChabadDashboard = () => {
