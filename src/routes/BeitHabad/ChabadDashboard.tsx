be9409625db5e34603c650d8e612c9893fda6ca4
TamamTamar <tamar.tamam770@gmail.com>
2024-11-27 12:19:46 +0200

    b

--- src/routes/BeitHabad/ChabadDashboard.tsx
+++ src/routes/BeitHabad/ChabadDashboard.tsx
@@ -5,7 +5,7 @@ import AdminParashot from "./AdminParashot";
 
 const ChabadDashboard = () => {
     return (
-        <Tabs aria-label="Tabs with icons" className="tabs rtl-tabs">
+        <Tabs dir="rtl" aria-label="Tabs with icons" className="tabs rtl-tabs">
             <Tabs.Item active title="נתוני פרשות" icon={FiBox}>
                 <AdminParashot />
             </Tabs.Item>
