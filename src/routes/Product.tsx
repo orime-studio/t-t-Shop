b3c4f338167ca2fb9d1d23b0a8f9af17f65c75b2
TamamTamar <tamar.tamam770@gmail.com>
2024-12-31 12:10:21 +0200

    product

--- src/routes/Product.tsx
+++ src/routes/Product.tsx
@@ -50,7 +50,7 @@ const Product = () => {
             return;
         }
         try {
-            await cart.addProductToCart(product._id, selectedVariant, product.title, 1, product.variants.find(v => v._id === selectedVariant)?.size || '', product.variants.find(v => v._id === selectedVariant)?.price || 0, mainImage);
+            await cart.addProductToCart(product._id, selectedVariant, product.title, 1, product.variants.find(v => v._id === selectedVariant)?.size || '', product.variants.find(v => v._id === selectedVariant)?.price || 0, mainImage, product.variants.find(v => v._id === selectedVariant)?.colors[0].name || '');
             navigate('/cart');
         } catch (error) {
             console.error('Failed to add product to cart.', error);
