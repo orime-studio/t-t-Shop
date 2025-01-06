c361729839f185658fc0d23b41d2022108aef989
TamamTamar <tamar.tamam770@gmail.com>
2025-01-06 12:14:47 +0200

    mobile

--- src/routes/homePage/Categories.tsx
+++ src/routes/homePage/Categories.tsx
@@ -8,23 +8,23 @@ const Categories = () => {
       <h2 className="categories-title">Shop by Category</h2>
       <div className="categories">
         <Link to="/?category=Bags" className="category">
-          <img src="img/homePage/BAGS.jpg" alt="BAGS" />
+          <img src="img/homePage/BAGS.png" alt="BAGS" />
           <h3 className="category-title">BAGS</h3>
         </Link>
         <Link to="/?category=Jacket" className="category">
-          <img src="img/homePage/TOPS.jpg" alt="TOPS" />
+          <img src="img/homePage/TOPS.png" alt="TOPS" />
           <h3 className="category-title">TOPS</h3>
         </Link>
         <Link to="/?category=Coats" className="category">
-          <img src="img/homePage/OUTERWEAR.jpg" alt="OUTERWEAR" />
+          <img src="img/homePage/OUTERWEAR.png" alt="OUTERWEAR" />
           <h3 className="category-title">OUTERWEAR</h3>
         </Link>
         <Link to="/?category=Shoes" className="category">
-          <img src="img/homePage/SHOES.jpg" alt="SHOES" />
+          <img src="img/homePage/SHOES.png" alt="SHOES" />
           <h3 className="category-title">SHOES</h3>
         </Link>
-        <Link to="/?category=Jewelry" className="category">
-          <img src="img/homePage/JEWELRY.jpg" alt="JEWELRY" />
+        <Link to="/?category=Jewelry" className="category category-jewelry">
+          <img src="img/homePage/JEWELRY.png" alt="JEWELRY" />
           <h3 className="category-title">JEWELRY</h3>
         </Link>
       </div>
