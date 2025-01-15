bdeab6baad2a13d3cc6d2e615f2aa8d77dbec2a6
sofiyaOsadchi <sofiyahason@gmail.com>
2025-01-15 15:24:01 +0200

    rrrr

--- src/routes/homePage/MainPage.tsx
+++ src/routes/homePage/MainPage.tsx
@@ -7,17 +7,18 @@ import './MainPage.scss';
 import FourProductsGallery from './ProductsLimited';
 import Bestsellers from './ProductsLimited';
 import CarouselComponent from '../../components/Carousel';
+import SaleBanner from './SaleBanner';
 
 const MainPage = () => {
     return (
         <div className="main-page-container">
             <MainBanner />
             <Categories />
-          
-            
+          <SaleBanner />
+          <FourProductsGallery />
             <Articles />
            <MainArticle />
-           <FourProductsGallery />
+           
         
        
         </div>
---
+++ src/routes/homePage/SaleBanner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SaleBanner.scss';
+
+const SaleBanner = () => {
+  return (
+    <div className="sale-banner-container">
+      <div className="sale-banner-content">
+        <h2 className="sale-banner-title">It's Cold Outside</h2>
+        <p className="sale-banner-description">Winter Sale <br/> 20% Off </p>
+        <div className="sale-banner-buttons">
+          <Link to="/shop" className="sale-banner-button">SHOP OUTERWEAR</Link>
+          <Link to="/learn-more" className="sale-banner-button">SHOP TOPS</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SaleBanner;
